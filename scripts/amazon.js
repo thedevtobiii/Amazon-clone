a7e7bb3bed043bff55d116696b43c7a997086fcf
DevTobi <fariuoyetola111@gmail.com>
2024-12-18 20:21:43 +0100

    made add to cart button interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -45,7 +45,7 @@ products.forEach((product) => {
                       Added
                     </div>
 
-                    <button class="add-to-cart-button button-primary">
+                    <button class="add-to-cart-button button-primary js-add-to-cart" data-product-id="${product.id}">
                       Add to Cart
                     </button>
                   </div>
@@ -55,9 +55,43 @@ console.log(productsHTML);
 /*
 NOTES: The way forEach works is that it takes each object, saves it in the parameter called product and the runs the code in the function, one object
 after the other.
-this generates the html for each object and therefore the entire product section  */
+this generates the html for each object and therefore the entire product section, added a data attribute  */
 
 //SECTION 3: PUTTING THE HTML ON THE WEBPAGE USING THE DOM
 //using the DOM, we are going to get the class for the parent container of the product-container to input the generated HTML in it
 document.querySelector('.js-products-grid').innerHTML= productsHTML;
-//we are basically saying all the Html that shoul exist in the parent container should be the only one we allow i.e the variable productsHTML!
+//we are basically saying all the Html that should exist in the parent container should be the only one we allow i.e the variable productsHTML!
+
+//SECTION 3: making the add to cart button interactive
+//NOTES: we got all the add to cart button using document.queryselectorAll and the class we added, then saved it in the button parameter then addeventlistener.
+
+
+document.querySelectorAll ('.js-add-to-cart')
+.forEach((button)=>{
+  button.addEventListener('click', ()=>{
+    const productId = button.dataset.productId; 
+    //revisit
+
+    let matchingItem;
+    cart.forEach((item)=> {
+      if (productId === item.productId){
+        matchingItem = item;
+
+      }
+
+    });
+    if (matchingItem){
+      matchingItem.quantity +=1
+    } else{
+      cart.push({
+        productId: productId,
+        quantity:1
+      }); 
+
+    }
+    //adding to cart
+     
+    console.log(cart);  
+  });
+});
+//now to check if product is already in cart, if it is we increase quantity by 1, if it isn't we add it to the cart by looping through our cart and look for a particular product name^
