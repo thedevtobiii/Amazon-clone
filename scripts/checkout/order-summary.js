faf12192ae19809e1df36e17c591628ff9e219ba
DevTobi <fariuoyetola11@gmail.com>
2024-12-25 16:11:41 +0100

    payment summary now works!

--- scripts/checkout/order-summary.js
+++ scripts/checkout/order-summary.js
@@ -1,8 +1,8 @@
 import { cart, removeFromCart,updateDeliveryOption } from "../../data/cart.js";
-import { products } from "../../data/products.js";
+import { products,getProduct } from "../../data/products.js";
 import { formatCurrency } from "../utilities/cash.js"; 
 import dayjs from "https://unpkg.com/dayjs@1.11.10/esm/index.js"  
-import { deliveryOptions } from "../../data/deliveryoptions.js"; 
+import { deliveryOptions, getDeliveryOption } from "../../data/deliveryoptions.js"; 
  
 const today = dayjs()
 const deliveryDate = today.add(7, 'days')   
@@ -14,21 +14,14 @@ export function renderOrderSummary (){
 let cartSummaryHTML ='';
 cart.forEach((cartItem)=>{
   const productId= cartItem.productId;
-  let matchingProduct;
-  products.forEach((product)=> {
-    if (product.id === productId) {
-      matchingProduct = product;        
 
-    }
-  });
+
+  const matchingProduct = getProduct(productId);//refer to product.js
+
 
   const deliveryOptionId = cartItem.deliveryOptionId;
-  let deliveryOption;
-  deliveryOptions.forEach((option)=>{
-    if (option.id===deliveryOptionId){
-      deliveryOption=option;
-    }
-  });
+  const deliveryOption = getDeliveryOption(deliveryOptionId);
+
   const today = dayjs();
   const deliveryDate = today.add(deliveryOption.deliveryDays, 'days');
   const dateString = deliveryDate.format ('dddd, MMMM D');
