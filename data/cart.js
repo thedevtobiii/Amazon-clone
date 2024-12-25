76d7fbf4698d436f93f4ee300283a65456d9ead2
DevTobi <fariuoyetola11@gmail.com>
2024-12-25 08:41:05 +0100

    delete button now actually deletes product

--- data/cart.js
+++ data/cart.js
@@ -1,9 +1,9 @@
-export const cart = [{
+export let cart = [{
   productId:"e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
   quantity: 2
 },{
   productId:"15b6fc6f-327a-4ec4-896f-486349e85a3d",
-  quantity: 2
+  quantity: 1
   //default data
 }
 
@@ -28,4 +28,16 @@ export function addToCart (productId){
       //IF IT IS ALREADY IN CART I.E MATCHING ITEM,INCREASE THE QUANTITY PROPERTY BY  1, ELSE PUSH THE PRODUCT(ID, QUANTITY) INTO THE CART ARRAY
 
     }
+}
+
+//function to delete item from cart
+export function removeFromCart (productId){
+ const newCart =[];
+ cart.forEach((cartItem)=>{
+  if(cartItem.productId !== productId) {
+  newCart.push(cartItem);
+  }
+
+ });
+ cart = newCart;
 }
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import { cart } from "../data/cart.js";
+import { cart, removeFromCart } from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utilities/cash.js"; 
 let cartSummaryHTML ='';
@@ -7,7 +7,7 @@ cart.forEach((cartItem)=>{
   let matchingProduct;
   products.forEach((product)=> {
     if (product.id === productId) {
-      matchingProduct = product;  
+      matchingProduct = product;        
 
     }
   });
@@ -35,7 +35,7 @@ cart.forEach((cartItem)=>{
           <span class="update-quantity-link link-primary">
             Update
           </span>
-          <span class="delete-quantity-link link-primary">
+          <span class="delete-quantity-link link-primary js-delete-link" data-product-id= "${matchingProduct.id}">
             Delete
           </span>
         </div>
@@ -89,4 +89,13 @@ cart.forEach((cartItem)=>{
   </div>
 `;
 });
+
 document.querySelector('.js-order-summary').innerHTML = cartSummaryHTML
+
+document.querySelectorAll('.js-delete-link').forEach((link)=>{
+  link.addEventListener('click',()=>{
+   const productId = link.dataset.productId;
+   removeFromCart(productId);
+  });
+
+});
