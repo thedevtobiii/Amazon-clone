d34b9e7c467ec4049cb8ac527f4b992cc890f2ab
DevTobi <fariuoyetola11@gmail.com>
2024-12-25 09:08:51 +0100

    saved cart to local storage.

--- data/cart.js
+++ data/cart.js
@@ -1,13 +1,25 @@
-export let cart = [{
-  productId:"e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-  quantity: 2
-},{
-  productId:"15b6fc6f-327a-4ec4-896f-486349e85a3d",
-  quantity: 1
-  //default data
+export let cart = JSON.parse(localStorage.getItem('cart'));
+
+if(!cart){
+  cart = [{
+    productId:"e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    quantity: 2
+  },{
+    productId:"15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    quantity: 1
+    //default data
+  }
+  
+  ];  //default value for empty cart  
+}
+   
+
+//function to save cart to local storage
+function saveToStorage(){
+  localStorage.setItem('cart', JSON.stringify(cart));
 }
 
-];           
+
 
 export function addToCart (productId){
   let matchingItem; //NEW VARIABLE FOR MATCHING ITEMS
@@ -28,6 +40,7 @@ export function addToCart (productId){
       //IF IT IS ALREADY IN CART I.E MATCHING ITEM,INCREASE THE QUANTITY PROPERTY BY  1, ELSE PUSH THE PRODUCT(ID, QUANTITY) INTO THE CART ARRAY
 
     }
+    saveToStorage();
 }
 
 //function to delete item from cart
@@ -40,4 +53,5 @@ export function removeFromCart (productId){
 
  });
  cart = newCart;
+ saveToStorage();
 }
