e47c437bfb2fc3802e3145c7fa23163cf2050ad2
DevTobi <fariuoyetola11@gmail.com>
2025-01-16 00:59:45 +0100

    inheritance

--- data/products.js
+++ data/products.js
@@ -31,6 +31,36 @@ class Product {
   return `$${formatCurrency(this.priceCents)}`
   }
 }
+//inheritance
+class Clothing extends Product{
+  sizeChartLink;
+
+  constructor(productDetails){
+super (productDetails);
+this.sizeChartLink = productDetails.sizeChartLink  }
+
+}
+const tshirt = new Clothing(
+  {
+    id: "83d4ca15-0f35-48f5-b7a3-1ea210004f2e",
+    image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
+    name: "Adults Plain Cotton T-Shirt - 2 Pack",
+    rating: {
+      stars: 4.5,
+      count: 56
+    },
+    priceCents: 799,
+    keywords: [
+      "tshirts",
+      "apparel",
+      "mens"
+    ],
+    type: "clothing",
+    sizeChartLink: "images/clothing-size-chart.png"
+  }
+);
+console.log(tshirt);
+console.log (tshirt.getPrice); 
 
 
 const product1 = new Product( { //new to generate new product class
