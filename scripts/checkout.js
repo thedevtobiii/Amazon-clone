76fa331c0120d5662f5b6273feca60e7e2e50b66
DevTobi <fariuoyetola11@gmail.com>
2025-01-15 23:02:36 +0100

    improved code with classes

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,6 @@
 import { renderOrderSummary } from "./checkout/order-summary.js";
 import { renderPaymentSummary } from "./checkout/payment-summary.js";
-import '../data/cart-oop.js';
+import '../data/cart-class.js';
 
 renderOrderSummary();  
 renderPaymentSummary();  
