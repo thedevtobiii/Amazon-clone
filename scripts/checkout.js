675a17ddf894587ba5a42e59f63b26f01d4c6e39
DevTobi <fariuoyetola11@gmail.com>
2025-01-18 20:58:57 +0100

    used promises

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,9 +1,54 @@
 import { renderOrderSummary } from "./checkout/order-summary.js";
 import { renderPaymentSummary } from "./checkout/payment-summary.js";
 import { loadProducts } from "../data/products.js";  
+import { loadCart } from "../data/cart.js";  
+
 // import '../data/cart-class.js';
 // import '../data/backend-prac.js';
-loadProducts(()=>{
-renderOrderSummary();  
-renderPaymentSummary();    
-});
+
+Promise.all([
+  new Promise((resolve)=>{
+    //runs the inner function immediately
+   //  console.log('promise');  
+   loadProducts(()=>{
+    
+     resolve('value1');   
+   });
+ }),
+ new Promise((resolve) =>{
+  loadCart(()=>{
+    resolve();
+  });
+})
+
+]).then((values)=>{
+  renderOrderSummary();  
+  renderPaymentSummary();   
+});   
+
+// new Promise((resolve)=>{
+ 
+//    //runs the inner function immediately
+//   //  console.log('promise');  
+//   loadProducts(()=>{
+   
+//     resolve('value1');   
+//   });
+// }).then((value)=>{
+//   console.log('value');
+//   return new Promise((resolve) =>{
+//     loadCart(()=>{
+//       resolve();
+//     });
+//   });
+// }).then(()=>{
+//   renderOrderSummary();  
+// renderPaymentSummary();    
+// });  
+
+// // loadProducts(()=>{
+// //   loadCart(()=>{
+// //     renderOrderSummary();  
+// // renderPaymentSummary();    
+// //   });
+// // });   
