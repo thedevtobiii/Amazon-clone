8d68effaa3d2ad719c404ced418642fb5873366a
DevTobi <fariuoyetola11@gmail.com>
2025-01-18 01:59:23 +0100

    loaded products from the backend

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,92 +1,96 @@
 import { cart, addToCart } from "../data/cart.js";  
-import { products } from "../data/products.js";   
+import { products, loadProducts} from "../data/products.js";   
 import { formatCurrency } from "./utilities/cash.js"; 
 
+loadProducts(renderProductsGrid);  
+
+function renderProductsGrid(){
 let productsHTML ='';  
 products.forEach((product) => {
   productsHTML += `
-              <div class="product-container">
-                    <div class="product-image-container">
-                      <img class="product-image"
-                        src="${product.image}">
-                    </div>
+                      <div class="product-container">
+                            <div class="product-image-container">
+                              <img class="product-image"
+                                src="${product.image}">
+                            </div>
 
-                    <div class="product-name limit-text-to-2-lines">
-                     ${product.name}   
-                    </div>
+                            <div class="product-name limit-text-to-2-lines">
+                            ${product.name}   
+                            </div>
 
-                    <div class="product-rating-container">
-                      <img class="product-rating-stars"
-                        src="${product.getStarsUrl()}">
-                      <div class="product-rating-count link-primary">
-                        ${product.rating.count}
-                      </div>      
-                    </div>
+                            <div class="product-rating-container">
+                              <img class="product-rating-stars"
+                                src="${product.getStarsUrl()}">
+                              <div class="product-rating-count link-primary">
+                                ${product.rating.count}
+                              </div>      
+                            </div>
 
-                    <div class="product-price">
-                       ${product.getPrice()}  
-                    </div>
+                            <div class="product-price">
+                              ${product.getPrice()}  
+                            </div>
 
-                    <div class="product-quantity-container">
-                      <select>
-                        <option selected value="1">1</option>
-                        <option value="2">2</option>
-                        <option value="3">3</option>
-                        <option value="4">4</option>
-                        <option value="5">5</option>
-                        <option value="6">6</option>
-                        <option value="7">7</option>
-                        <option value="8">8</option>
-                        <option value="9">9</option>
-                        <option value="10">10</option>
-                      </select>
-                    </div>  
+                            <div class="product-quantity-container">
+                              <select>
+                                <option selected value="1">1</option>
+                                <option value="2">2</option>
+                                <option value="3">3</option>
+                                <option value="4">4</option>
+                                <option value="5">5</option>
+                                <option value="6">6</option>
+                                <option value="7">7</option>
+                                <option value="8">8</option>
+                                <option value="9">9</option>
+                                <option value="10">10</option>
+                              </select>
+                            </div>  
 
-                    ${product.extraInfoHTML()}    
+                            ${product.extraInfoHTML()}    
 
-                    <div class="product-spacer"></div>
+                            <div class="product-spacer"></div>
 
-                    <div class="added-to-cart">
-                      <img src="images/icons/checkmark.png">
-                      Added
-                    </div>
+                            <div class="added-to-cart">
+                              <img src="images/icons/checkmark.png">
+                              Added
+                            </div>
 
-                    <button class="add-to-cart-button button-primary js-add-to-cart" data-product-id="${product.id}">
-                      Add to Cart
-                    </button>
-                  </div>
-  `
-});
-console.log(productsHTML); 
-/*
-NOTES: The way forEach works is that it takes each object, saves it in the parameter called product and the runs the code in the function, one object
-after the other.
-this generates the html for each object and therefore the entire product section, added a data attribute  */
+                            <button class="add-to-cart-button button-primary js-add-to-cart" data-product-id="${product.id}">
+                              Add to Cart
+                            </button>
+                          </div>
+          `
+        });
+        console.log(productsHTML); 
+        /*
+        NOTES: The way forEach works is that it takes each object, saves it in the parameter called product and the runs the code in the function, one object
+        after the other.
+        this generates the html for each object and therefore the entire product section, added a data attribute  */
 
-//SECTION 3: PUTTING THE HTML ON THE WEBPAGE USING THE DOM
-//using the DOM, we are going to get the class for the parent container of the product-container to input the generated HTML in it
-document.querySelector('.js-products-grid').innerHTML= productsHTML;
-//we are basically saying all the Html that should exist in the parent container should be the only one we allow i.e the variable productsHTML!
+        //SECTION 3: PUTTING THE HTML ON THE WEBPAGE USING THE DOM
+        //using the DOM, we are going to get the class for the parent container of the product-container to input the generated HTML in it
+        document.querySelector('.js-products-grid').innerHTML= productsHTML;
+        //we are basically saying all the Html that should exist in the parent container should be the only one we allow i.e the variable productsHTML!
 
-//SECTION 4: making the add to cart button interactive
-//NOTES: we got all the add to cart button using document.queryselectorAll and the class we added, then saved it in the button parameter then addeventlistener.
+        //SECTION 4: making the add to cart button interactive
+        //NOTES: we got all the add to cart button using document.queryselectorAll and the class we added, then saved it in the button parameter then addeventlistener.
 
 
-function updateCartQuantity (){
- //calculating cart total quantity
- let cartQuantity = 0;
- cart.forEach((cartItem)=>{
-  cartQuantity += cartItem.quantity;
+        function updateCartQuantity (){
+        //calculating cart total quantity
+        let cartQuantity = 0;
+        cart.forEach((cartItem)=>{
+          cartQuantity += cartItem.quantity;
 
- }) ;  
-document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+        }) ;  
+        document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+        }
+
+        document.querySelectorAll ('.js-add-to-cart')
+        .forEach((button)=>{
+          button.addEventListener('click', ()=>{
+            const productId = button.dataset.productId; //gets the data attribute value and saves it inside productID
+            addToCart(productId);
+            updateCartQuantity();
+          });
+        });
 }
-
-document.querySelectorAll ('.js-add-to-cart')
-.forEach((button)=>{
-  button.addEventListener('click', ()=>{
-    const productId = button.dataset.productId; //gets the data attribute value and saves it inside productID
-    addToCart(productId);
-    updateCartQuantity();
-  });
-});
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,7 +1,9 @@
 import { renderOrderSummary } from "./checkout/order-summary.js";
 import { renderPaymentSummary } from "./checkout/payment-summary.js";
+import { loadProducts } from "../data/products.js";  
 // import '../data/cart-class.js';
 // import '../data/backend-prac.js';
-
+loadProducts(()=>{
 renderOrderSummary();  
-renderPaymentSummary();  
+renderPaymentSummary();    
+});
